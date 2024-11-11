0e284a9c615e01ec3ed8612f71828276a70fe298
Adeniyi Adebayo <jasonzenith2017@gmail.com>
2024-11-11 05:19:54 -0500

    first commit

---
+++ Grin/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.ComponentModel;
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography;
+
+namespace MyApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         // Number(40);
+          Program.Add(10, 20);
+        }
+
+       static int Add(int FN, int SN)
+        {
+          return FN + SN;
+        }
+
+        static void Number(int Target)
+        {
+          int Start = 0;
+          while (Start <= Target)
+          {
+            Console.WriteLine(Start);
+            Start = Start + 2;
+          }
+
+        }
+
+        }
+    
+    }
